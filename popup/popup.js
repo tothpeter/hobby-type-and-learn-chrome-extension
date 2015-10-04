ce745e6fd9e233b37f4c2d76dd4689e16d4dcc75
Peter Toth <tothpeter08@gmail.com>
2015-10-04 20:22:59 +0200

    Add session class skeleton with some basic functions and refactor a bit

--- popup/popup.js
+++ popup/popup.js
@@ -1,34 +1,74 @@
 var statusDisplay = null;
 
+var Session = function() {
+
+  this.create = function create(token, user) {
+    localStorage.token = token;
+    localStorage.user = JSON.stringify(user);
+  }
+
+  this.currentUser = function currentUser() {
+    if(localStorage.user === undefined){
+      return null;
+    }
+    else {
+      return JSON.parse(localStorage.user);
+    }
+  }  
+
+  this.isAuthenticated = function isAuthenticated() {
+    return localStorage.token !== undefined;
+  }
+
+  this.authenticate = function() {
+    
+  }
+
+  this.invalidate = function() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+  
+  return {
+    authenticate: authenticate,
+    create: create,
+    isAuthenticated: isAuthenticated,
+    invalidate: invalidate,
+    currentUser: currentUser
+  }
+}();
+
 function login() {
-    event.preventDefault();
+  event.preventDefault();
 
-    var postUrl = 'http://type-and-learn-api.dev/sessions/login_chrome_plugin',
-        xhr = new XMLHttpRequest();
-    
-    var email = encodeURIComponent(document.getElementById('email').value),
-        password = encodeURIComponent(document.getElementById('password').value);
+  var postUrl = 'http://type-and-learn-api.dev/sessions/login_chrome_plugin',
+      xhr = new XMLHttpRequest();
+  
+  var email = encodeURIComponent(document.getElementById('email').value),
+      password = encodeURIComponent(document.getElementById('password').value);
 
-    var params = 'user[email]=' + email + '&user[password]=' + password;
+  var params = 'user[email]=' + email + '&user[password]=' + password;
 
-    xhr.open('POST', postUrl, true);
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        statusDisplay.innerHTML = '';
-        if (xhr.status >= 200 && xhr.status < 300) {
-          var response = JSON.parse(xhr.response);
-          // console.log(response.user);
-        }
-        else {
-          statusDisplay.innerHTML = 'Error: ' + xhr.statusText;
-        }
+  xhr.open('POST', postUrl, true);
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      statusDisplay.innerHTML = '';
+      if (xhr.status >= 200 && xhr.status < 300) {
+        var response = JSON.parse(xhr.response);
+        console.log(Session.isAuthenticated());
+        Session.create(response.token, response.user);
+        // console.log(response.user);
       }
-    };
-    
-    xhr.send(params);
-    statusDisplay.innerHTML = 'Working...';
+      else {
+        statusDisplay.innerHTML = 'Error: ' + xhr.statusText;
+      }
+    }
+  };
+  
+  xhr.send(params);
+  statusDisplay.innerHTML = 'Working...';
 }
 
 window.addEventListener('load', function(evt) {
