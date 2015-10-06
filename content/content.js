44036d4a6629f320e662f1f5a35b526f1d527d07
Peter Toth <tothpeter08@gmail.com>
2015-10-06 17:57:22 +0200

    Handle basic form submit event

--- content/content.js
+++ content/content.js
@@ -15,11 +15,61 @@ var newCardPopup = {
 
       this.root = this.$popup.createShadowRoot();
       this.root.innerHTML = html;
-      // this.root.innerHTML = "HTMLString: " + selectedText;
 
       document.body.appendChild(this.$popup);
+
+      this.bindEvents();
     }
+  },
+
+  bindEvents: function() {
+    var $wrapper = $(this.root).find('#new-card-popup-wrapper');
+    
+    $wrapper.submit(function(event) {
+      event.preventDefault();
+      var params = {
+        sideA: $wrapper.find('#input-side-a').val(),
+        sideB: $wrapper.find('#input-side-b').val(),
+        proficiencyLevel: $wrapper.find('#input-proficiency-level').val()
+      }
+
+      Card.create(params);
+    });
   }
 }
 
-// newCardPopup.init();
+var Adapter = {
+  host: 'http://type-and-learn-api.dev'
+}
+
+var Card = {
+  create: function(params) {
+    chrome.storage.sync.get({
+      token: null
+    }, function(items) {
+      $.ajax({
+        method: 'post',
+        url: Adapter.host + '/cards',
+        data: {data: {attributes: params}},
+        beforeSend: function(request) {
+          request.setRequestHeader('Authorization', 'Token token="' + items.token + '"');
+        }
+      })
+      .done(function() {
+        console.log("success");
+      })
+      .fail(function(xhr) {
+        if (xhr.status === 422) {
+          var response = JSON.parse(xhr.responseText);
+          alert('Validation error');
+        }
+        else {
+          alert('Something went wrong on our servers, please try later.');
+        }
+      })
+      .always(function() {
+        console.log("complete");
+      });
+    });
+  }
+}
