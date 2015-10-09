358d8b0d1bcf411ab27eb30be3d93fa01d48677b
Peter Toth <tothpeter08@gmail.com>
2015-10-09 20:36:56 +0200

    Using grunt to insert inline css into the background page from file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,18 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    processhtml: {
+      options: {
+        // Task-specific options go here.
+      },
+      dev: {
+        files: {
+          'background/index_generated.html': ['background/index_template.html']
+        }
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-processhtml');
 };
---
+++ README.md
@@ -0,0 +1,11 @@
+# Type and Learn Chrome Plugin
+
+A broader description is on its way...
+
+## Prerequisites
+* [Git](http://git-scm.com/)
+* [Node.js](http://nodejs.org/) (with NPM)
+
+## Installation for development
+* `npm install`
+* `grunt processhtml`
