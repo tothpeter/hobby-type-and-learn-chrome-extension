6864aac4c05e1f74e9e68e642745fc252420d775
Peter Toth <tothpeter08@gmail.com>
2015-10-12 21:34:20 +0200

    Change alert messages to notification popup (alert closed it)

--- popup/popup.js
+++ popup/popup.js
@@ -9,6 +9,7 @@ Session.isAuthenticated().then(
 
 $('#login-form').submit(function(event) {
   event.preventDefault();
+  Notification.hide();
 
   const credentials = {
     email: $('#email').val(),
@@ -22,20 +23,40 @@ $('#login-form').submit(function(event) {
     },
     function(xhr) {
       if (xhr.status === 401) {
-        alert('Invalid email or password');
+        $('#email').focus();
+        Notification.error('Invalid email or password.');
       }
       else if (xhr.status >= 500) {
-        alert('Something went wrong on our servers, please try later.');
+        Notification.error('Something went wrong on our servers, please try later.');
       }
       else {
-        alert('Something went wrong, please try later.');
+        Notification.error('Something went wrong, please try later.');
       }
     }
   );
 });
+
+$('body').on('keydown', function() {
+  Notification.hide();
+});
+
 
 $('#btn-log-out').click(function() {
   Session.invalidate();
   $('.panel-login').addClass('active');
   $('.panel-logged-in').removeClass('active');
 });
+
+var Notification = {
+  $container: $('#notification-wrapper'),
+  $message: $('#notification-wrapper .message'),
+
+  error: function(message) {
+    this.$message.text(message);
+    this.$container.addClass('error visible');
+  },
+
+  hide: function() {
+    this.$container.removeClass('visible');
+  }
+}
